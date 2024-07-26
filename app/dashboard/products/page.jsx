3a26f127ce1c7fe229753ac6b16e1b2a939546f0
veerabala <imveerabala@gmail.com>
2024-07-26 15:51:39 +0530

    new pages added

--- app/dashboard/products/page.jsx
+++ app/dashboard/products/page.jsx
@@ -1,9 +1,92 @@
+import Pagination from "@/app/ui/dashboard/pagination/pagination";
+import Search from "@/app/ui/dashboard/search/search";
+import Link from "next/link";
+import Image from "next/image";
+import styles from "@/app/ui/dashboard/users/users.module.css";
+
 function ProductsPage() {
-    return (  
-        <div>
-            Productpage
-        </div>
-    );
+  const products = [];
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder="Search for Product" />
+        <Link href="/dashboard/users/add">
+          <button className={styles.addbutton}>Add New</button>
+        </Link>
+      </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Title</td>
+            <td>Description</td>
+            <td>Price</td>
+            <td>Created at</td>
+            <td>Stock</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>iphone</td>
+            <td>broken</td>
+            <td>23424</td>
+            <td>India</td>
+            <td>34</td>
+            <td>
+              <div className={styles.buttons}>
+                <Link href={`/dashboard/users`}>
+                  <button className={`${styles.button} ${styles.view}`}>
+                    View
+                  </button>
+                </Link>
+                {/* <form action={deleteProduct}> */}
+                <input type="hidden" name="id"  />
+                <button className={`${styles.button} ${styles.delete}`}>
+                  Delete
+                </button>
+              </div>
+            </td>
+          </tr>
+          {products.map((product) => (
+            <tr key={product.id}>
+              <td>
+                <div className={styles.product}>
+                  <Image
+                    src={product.img || "/avatar.png"}
+                    alt=""
+                    width={40}
+                    height={40}
+                    className={styles.productImage}
+                  />
+                  {product.username}
+                </div>
+              </td>
+              <td>{product.email}</td>
+              <td>{product.createdAt?.toString().slice(4, 16)}</td>
+              <td>{product.isAdmin ? "Admin" : "Client"}</td>
+              <td>{product.isActive ? "active" : "passive"}</td>
+              <td>
+                <div className={styles.buttons}>
+                  <Link href={`/dashboard/users/${product.id}`}>
+                    <button className={`${styles.button} ${styles.view}`}>
+                      View
+                    </button>
+                  </Link>
+                  <form action={deleteProduct}>
+                    <input type="hidden" name="id" value={product.id} />
+                    <button className={`${styles.button} ${styles.delete}`}>
+                      Delete
+                    </button>
+                  </form>
+                </div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <Pagination />
+    </div>
+  );
 }
 
 export default ProductsPage;
--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -1,9 +1,92 @@
+import styles from "@/app/ui/dashboard/users/users.module.css";
+import Search from "../../ui/dashboard/search/search";
+import Link from "next/link";
+import Image from "next/image";
+import Pagination from "@/app/ui/dashboard/pagination/pagination";
+
 function Users() {
-    return ( 
-        <p>
-            usespage
-        </p>
-     );
+    const users = []
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder="Search for User" />
+        <Link href="/dashboard/users/add">
+          <button className={styles.addbutton}>Add New</button>
+        </Link>
+      </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Created At</td>
+            <td>Role</td>
+            <td>Status</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>Veerabala</td>
+            <td>vb@gmail.com</td>
+            <td>kumbakonam</td>
+            <td>Admin</td>
+            <td>active</td>
+            <td>
+              <div className={styles.buttons}>
+                <Link href={`/dashboard/users/test`}>
+                  <button className={`${styles.button} ${styles.view}`}>
+                    View
+                  </button>
+                </Link>
+                {/* <form action={deleteProduct}> */}
+                <input type="hidden" name="id"  />
+                <button className={`${styles.button} ${styles.delete}`}>
+                  Delete
+                </button>
+              </div>
+            </td>
+          </tr>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>
+                <div className={styles.user}>
+                  <Image
+                    src={user.img || "/avatar.png"}
+                    alt=""
+                    width={40}
+                    height={40}
+                    className={styles.userImage}
+                  />
+                  {user.username}
+                </div>
+              </td>
+              <td>{user.email}</td>
+              <td>{user.createdAt?.toString().slice(4, 16)}</td>
+              <td>{user.isAdmin ? "Admin" : "Client"}</td>
+              <td>{user.isActive ? "active" : "passive"}</td>
+              <td>
+                <div className={styles.buttons}>
+                  <Link href={`/dashboard/users/${user.id}`}>
+                    <button className={`${styles.button} ${styles.view}`}>
+                      View
+                    </button>
+                  </Link>
+                  <form action={deleteUser}>
+                    <input type="hidden" name="id" value={user.id} />
+                    <button className={`${styles.button} ${styles.delete}`}>
+                      Delete
+                    </button>
+                  </form>
+                </div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <Pagination />
+    </div>
+  );
 }
 
 export default Users;
